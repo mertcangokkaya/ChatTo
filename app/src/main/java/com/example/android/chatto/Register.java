3831826569f5ac28b7bb76f682ad86d6564600c6
mertcang <mertcan.gokkaya@ogr.sakarya.edu.tr>
2017-12-15 16:34:14 +0300

    Firebase bağlantısı kuruldu

---
+++ app/src/main/java/com/example/android/chatto/Register.java
@@ -0,0 +1,33 @@
+package com.example.android.chatto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+
+
+
+public class  Register extends AppCompatActivity {
+    EditText username, password;
+    Button registerButton;
+    String user, pass;
+    TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Firebase.setAndroidContext(this);
+
+
+
+
+
+
+    }
+}
