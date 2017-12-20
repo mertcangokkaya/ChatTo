f17e99a5a856ac839b9c1c6675f1aec815508a04
AHMET586 <darendeli586@gmail.com>
2017-12-20 13:51:47 +0300

    Update MyApplication.java

--- app/src/main/java/com/example/android/chatto/MyApplication.java
+++ app/src/main/java/com/example/android/chatto/MyApplication.java
@@ -8,4 +8,22 @@ import com.onesignal.OneSignal;
 public class MyApplication extends android.app.Application {
 
     
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Previous versions of Firebase
+        Firebase.setAndroidContext(this);
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+        // Call syncHashedEmail anywhere in your app if you have the user's email.
+        // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+        // OneSignal.syncHashedEmail(userEmail);
+
+        }
+
+    
     }
